4ffe14cc9ef7b04ecd50c630c54fe8bb2aaa382c
Aldo Noyola <xarra05@yahoo.com.mx>
2016-10-30 20:35:27 +0000

    Create relation user stock, add constraints and limits

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+  
   get 'stocks/search'
 
   get 'stock/search'
 
   devise_for :users
+  resources :user_stocks, except: [:show,:edit,:update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
   get 'my_portfolio', to: "users#my_portfolio"
   get 'search_stocks', to: "stocks#search"
+  
 end
